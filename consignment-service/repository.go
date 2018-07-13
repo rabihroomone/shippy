7a4dc9092efe9d8b8e62e8c9166d8863aec67e5b
Rabieh Fashwall <rabih@roomone.com>
2018-07-13 13:28:15 +0300

    Add datastores

---
+++ consignment-service/repository.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	pb "github.com/rabihroomone/shippy/consignment-service/proto/consignment"
+	"gopkg.in/mgo.v2"
+)
+
+const (
+	dbName                = "shippy"
+	consignmentCollection = "consignments"
+)
+
+type Repository interface {
+	Create(*pb.Consignment) error
+	GetAll() ([]*pb.Consignment, error)
+	Close()
+}
+
+type ConsignmentRepository struct {
+	session *mgo.Session
+}
+
+func (repo *ConsignmentRepository) Create(c *pb.Consignment) error {
+	return repo.collection().Insert(c)
+}
+func (repo *ConsignmentRepository) GetAll() ([]*pb.Consignment, error) {
+	var consignments []*pb.Consignment
+	// Find normally takes a query, but as we want everything, we can nil this.
+	// We then bind our consignments variable by passing it as an argument to .All().
+	// That sets consignments to the result of the find query.
+	// There's also a `One()` function for single results.
+	err := repo.collection().Find(nil).All(&consignments)
+	return consignments, err
+}
+
+func (repo *ConsignmentRepository) Close() {
+	repo.session.Close()
+}
+
+func (repo *ConsignmentRepository) collection() *mgo.Collection {
+	return repo.session.DB(dbName).C(consignmentCollection)
+}
